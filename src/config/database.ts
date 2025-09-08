087d78a824ea48859a5c486b213fef07650130d2
Vinicius <viniciusff200@gmail.com>
2025-09-08 15:28:59 -0300

    adicionando SSL a configuração do banco de dados

--- src/config/database.ts
+++ src/config/database.ts
@@ -25,6 +25,9 @@ import { ProductColor } from '../models/product-color.model';
 export const sequelize = new Sequelize(process.env.DATABASE_URL!, {
   dialect: "postgres",
   logging: false,
+  dialectOptions: {
+    ssl: { require: true, rejectUnauthorized: false },
+  },
   models: [
     User,
     Product,
