eff32ebabe1f0824597db7eff3fde4dfcfba0fb3
Vinicius <viniciusff200@gmail.com>
2025-09-28 19:47:05 -0300

    Resolvendo o erro do dirname no modulo de uploadFeatures

--- src/admin/uploadFeatures.ts
+++ src/admin/uploadFeatures.ts
@@ -1,7 +1,12 @@
 // src/admin/uploadFeatures.ts
 
 import uploadFeature from '@adminjs/upload'
-import path from 'path'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const uploadPath = path.join(__dirname, '..', '..', 'public', 'uploads')
 
